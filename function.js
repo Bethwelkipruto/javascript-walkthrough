861e9c4eff9a33d31922c8c80bb9fa4426b739ce
bethwelkipruto <bethwel.c7@gmail.com>
2025-04-17 11:55:53 +0300

    add changes

---
+++ function.js
@@ -0,0 +1,13 @@
+
+function vehicles(name)
+{console.log("This car name is " + name)}
+
+{
+    vehicles("bmw");
+    vehicles("toyota");
+    vehicles("hdg");
+
+}
+console.log(vehicles)
+
+"This is a string".concat("!!!")
